16e6bdec3a7961e9d392c6e55d2d1f3e5a2bdcac
halimsa <halimsamodra2009@gmail.com>
2026-04-13 09:58:50 +0700

    commit project

--- api/index.js
+++ api/index.js
@@ -3,3 +3,12 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.use('/*', serverstatic({root: './public'}));
+
+app.get('./*', serverstatic({root: './public'}))
+
+app.onError((err, c) => {
+    console.error(`$(err)`);
+    return c.json({error: 'internal server error'}, 500)
+});
+
+export default app;
